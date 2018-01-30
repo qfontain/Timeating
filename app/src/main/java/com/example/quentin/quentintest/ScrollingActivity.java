eba4ea409176da8c74ccb3111fa9a6bbd8386d0a
Quentin FONTAINE <quentin.geof@hotmail.com>
2018-01-30 15:53:30 +0100

    Changements

--- app/src/main/java/com/example/quentin/quentintest/ScrollingActivity.java
+++ app/src/main/java/com/example/quentin/quentintest/ScrollingActivity.java
@@ -1,10 +1,22 @@
 package com.example.quentin.quentintest;
 
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.RingtoneManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.os.SystemClock;
+import android.support.v4.app.NotificationCompat;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.Calendar;
+import java.util.Date;
+
 
 public class ScrollingActivity extends Activity {
     Button S_comme_sushi;
@@ -29,7 +41,12 @@ public class ScrollingActivity extends Activity {
         Burger_King_La_Defense=findViewById(R.id.button19);
         Vapiano_La_Defense=findViewById(R.id.button20);
         Paradis_du_Fruit=findViewById(R.id.button21);
-
+        long time_millis =System.currentTimeMillis();
+        long total_secondes = time_millis/1000;
+        final int seconde =  (int) (total_secondes-total_secondes%3600);
+        long total_minutes = total_secondes/60;
+        int hour = (int)total_minutes%60;
+        final int minutes = (int) (total_minutes -hour*60);
         S_comme_sushi.setOnClickListener(new View.OnClickListener() { // même principe que dans l'activité 1 pour passer à l'activité suivante
             @Override
             public void onClick(View view) {
@@ -126,5 +143,28 @@ public class ScrollingActivity extends Activity {
             }
 
         });
+    }
+    public void scheduleNotification(Context context, long delay, int notificationId) {//delay is after how much time(in millis) from current time you want to schedule the notification
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                .setContentTitle("Départ du restaurant dans 5 minutes !")
+                .setContentText("")
+                .setAutoCancel(true)
+                .setSmallIcon(R.drawable.splash_screen)
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION));
+
+        Intent intent = new Intent(context, ScrollingActivity.class);
+        PendingIntent activity = PendingIntent.getActivity(context, notificationId, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+        builder.setContentIntent(activity);
+
+        Notification notification = builder.build();
+
+        Intent notificationIntent = new Intent(context, MyNotificationPublisher.class);
+        notificationIntent.putExtra(MyNotificationPublisher.NOTIFICATION_ID, notificationId);
+        notificationIntent.putExtra(MyNotificationPublisher.NOTIFICATION, notification);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, notificationId, notificationIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        long futureInMillis = SystemClock.elapsedRealtime() + delay;
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, futureInMillis, pendingIntent);
     }
 }
