c89c832090e84fc18514a53ca9b5655abf9ece9a
Quentin FONTAINE <quentin.geof@hotmail.com>
2018-02-08 23:52:01 +0100

    Intégration de l'activité de Muriel

---
+++ app/src/main/java/com/example/quentin/quentintest/Create_account.java
@@ -0,0 +1,13 @@
+package com.example.quentin.quentintest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Create_account extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
