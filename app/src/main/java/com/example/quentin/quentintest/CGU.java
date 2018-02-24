760d2f0e8c5981ecba255a7d8b4cc30ba4e04e08
Quentin FONTAINE <quentin.geof@hotmail.com>
2018-02-24 01:34:10 +0100

    Réalisation des activités CGU, Confidentialité et Contact de Marie pour le Navigation Drawer

---
+++ app/src/main/java/com/example/quentin/quentintest/CGU.java
@@ -0,0 +1,13 @@
+package com.example.quentin.quentintest;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class CGU extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cgu);
+    }
+}
